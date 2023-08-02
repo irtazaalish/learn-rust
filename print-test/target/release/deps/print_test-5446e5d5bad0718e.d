d1b27911b56f4c805cc44523c521c959b79dbf00
ali.sh.dl.vm.kali_all <irtazaalish@gmail.com>
2023-08-02 17:39:24 +1000

    added test-2 and the useful commands

---
+++ print-test/target/release/deps/print_test-5446e5d5bad0718e.d
@@ -0,0 +1,5 @@
+/home/lab/rust/learn-rust/print-test/target/release/deps/print_test-5446e5d5bad0718e: src/main.rs
+
+/home/lab/rust/learn-rust/print-test/target/release/deps/print_test-5446e5d5bad0718e.d: src/main.rs
+
+src/main.rs:
---
+++ print-test/target/release/print-test.d
@@ -0,0 +1,1 @@
+/home/lab/rust/learn-rust/print-test/target/release/print-test: /home/lab/rust/learn-rust/print-test/src/main.rs
